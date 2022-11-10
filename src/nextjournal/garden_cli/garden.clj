c5929e8580c52db2aed9b54fde16a81848d58b89
Paolo Holinski <paolo@nextjournal.com>
2022-11-10 17:09:10 +0100

    stream progress

--- src/nextjournal/garden_cli/garden.clj
+++ src/nextjournal/garden_cli/garden.clj
@@ -1,17 +1,49 @@
 (ns nextjournal.garden-cli.garden
-  (:require [babashka.process :as p]
-            [clojure.java.browse :as browse]))
+  (:require [nextjournal.garden-cli.util :as util]
+            [clojure.java.browse :as browse]
+            [java-http-clj.websocket :as ws]
+            [babashka.curl :as http]))
 
+(defn- parse-ws-urls [html]
+  (into #{} (re-seq #"wss?://[^\"]+" html)))
 
-(defn build [{:keys [env repo branch update]
-              :or {update true}}]
+(defn- stream-progress [ws-urls]
+  (let [promises (for [ws-url ws-urls]
+                   (let [p (promise)]
+                     (util/retry (fn []
+                                   (ws/build-websocket
+                                    ws-url
+                                    {:on-close (fn [ws status reason]
+                                                 (deliver p nil))
+                                     :on-error (fn [ws error]
+                                                 (deliver p nil))
+                                     :on-text (fn [ws text last?]
+                                                (print text)
+                                                (flush))})
+                                   {:success true}))
+                     p))]
+    (reduce (fn [a v] @v) nil promises)))
+
+(defn- show-build-progress [url]
+  (let [{:keys [status err body]} (http/get url)]
+    (when (= 200 status)
+      (stream-progress (parse-ws-urls body)))))
+
+(defn build [{:keys [env repo branch update browse show-progress]
+              :or {update true
+                   browse true
+                   show-progress true}}]
   (let [host (case env
-               :production "github.clerk.garden"
-               :staging "github.staging.clerk.garden")
+               :production "https://github.clerk.garden"
+               :staging "https://github.staging.clerk.garden"
+               :dev "http://localhost:8001")
         url (if branch
-              (format "https://%s/%s/tree/%s" host repo branch)
-              (format "https://%s/%s" host repo))
+              (format "%s/%s/tree/%s" host repo branch)
+              (format "%s/%s" host repo))
         url (if update
               (str url "?update=1")
               url)]
-    (browse/browse-url url)))
+    (when browse
+      (browse/browse-url url))
+    (when show-progress
+      (show-build-progress url))))
