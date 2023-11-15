722a88b6e676051f952a363661090a34e7277feb
Sohalt <paolo@nextjournal.com>
2023-11-15 14:12:58 +0100

    Ship binary in homebrew formula (#111)
    * Ship our own babashka in brew formula
    
    * Don't ship a cask

--- brew/update-formula.sh
+++ brew/update-formula.sh
@@ -3,13 +3,19 @@ set -ex
 PAT="$1"
 template="$(readlink -f "$(dirname "$0")/garden-formula.template")"
 VERSION="$(cat "$(readlink -f "$(dirname "$0")/../resources/VERSION")")"
-SHA256="$(curl -L "https://github.com/nextjournal/garden-cli/releases/download/v${VERSION}/garden.tar.gz.sha256")"
+LINUX_ARM_SHA="$(curl -L "https://github.com/nextjournal/garden-cli/releases/download/v${VERSION}/garden-linux-amd64-static.tar.gz.sha256")"
+LINUX_AMD_SHA="$(curl -L "https://github.com/nextjournal/garden-cli/releases/download/v${VERSION}/garden-linux-amd64-static.tar.gz.sha256")"
+MACOS_ARM_SHA="$(curl -L "https://github.com/nextjournal/garden-cli/releases/download/v${VERSION}/garden-macos-aarch64.tar.gz.sha256")"
+MACOS_AMD_SHA="$(curl -L "https://github.com/nextjournal/garden-cli/releases/download/v${VERSION}/garden-macos-amd64.tar.gz.sha256")"
 cd "$(mktemp -d)"
 git clone "https://x-access-token:${PAT}@github.com/nextjournal/homebrew-brew"
 cd homebrew-brew
 cp "$template" garden.rb
 sed -i "s/\${VERSION}/${VERSION}/g" garden.rb
-sed -i "s/\${SHA256}/${SHA256}/g" garden.rb
+sed -i "s/\${LINUX_ARM_SHA}/${LINUX_ARM_SHA}/g" garden.rb
+sed -i "s/\${LINUX_AMD_SHA}/${LINUX_AMD_SHA}/g" garden.rb
+sed -i "s/\${MACOS_ARM_SHA}/${MACOS_ARM_SHA}/g" garden.rb
+sed -i "s/\${MACOS_AMD_SHA}/${MACOS_AMD_SHA}/g" garden.rb
 git config user.name "Update"
 git config user.email "nextjournal@users.noreply.github.com"
 git add garden.rb
